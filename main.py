4c3acf67754bf00731a80d373fa57bfbaf51f919
Simon Morley <simon@polkaspots.com>
2017-03-06 18:29:14 +0100

    Stash

--- main.py
+++ main.py
@@ -13,31 +13,35 @@ def main(project_id, instance_id, table_id):
     instance = client.instance(instance_id)
     table = instance.table(table_id)
     column_family_id = 'ST'
-    column_id = 'TX_BYTES'.encode('utf-8')
+    # column_id = 'TX_BYTES'.encode('utf-8')
 
     print('Getting a single greeting by row key.')
 
     # [START scanning_all_rows]
     print('Scanning for all greetings:')
 
-    col1_filter = ColumnQualifierRegexFilter(b'TX_BYTES:*')
-    chain1 = RowFilterChain(filters=[col1_filter])
+    col1_filter = ColumnQualifierRegexFilter(b'TX_BYTES:([a-zA-Z0-9]{12})')
+    # col1_filter = ColumnQualifierRegexFilter(b'TX_BYTES:7081EB6E6051')
+    # chain1 = RowFilterChain(filters=[col1_filter])
 
+    # partial_rows = table.read_rows()
     partial_rows = table.read_rows(filter_=col1_filter)
-    # partial_rows = table.read_rows(filter_=col1_filter)
     partial_rows.consume_all()
 
     a = []
 
     for row_key, row in partial_rows.rows.items():
         key = row_key.decode('utf-8')
-        cell = row.cells[column_family_id][column_id][0]
+        cell = row.cells[column_family_id]
+        cell = cell[cell.keys()[0]][0]
         value = cell.value.decode('utf-8')
         val = { "Date": cell.timestamp.strftime("%a, %d %b %Y %H:%M:%S"), "Value": float(value) }
         a.append(val)
 
 
     print(a)
+
+    return
 
     incomplete_data = json_to_dataframe(a)
 
@@ -46,21 +50,21 @@ def main(project_id, instance_id, table_id):
     incomplete_data.set_index(['Date'], inplace=True)
 
     # problem_data = incomplete_data.sort_index().reindex(full_range)
-    print(incomplete_data.head(100))
+    # print(incomplete_data.head(100))
     # print(problem_data.head(100))
 
     axis = incomplete_data['Value']#.plot(kind='bar')
     upsampled = axis.resample('5T').mean()
     interpolated = upsampled.interpolate(method='time')
-    print(interpolated.head(100))
+    # print(interpolated.head(100))
 
     interpolated.plot(kind="line")
     # axis.set_ylim(18,22)
 
     # a = interpolated.reset_index().to_json(orient='records')
-    a = interpolated.to_json(orient='records')
+    # a = interpolated.to_json(orient='records')
 
-    print(a)
+    # print(a)
 
     # pyplot.show()
 
