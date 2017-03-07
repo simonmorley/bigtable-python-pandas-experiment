b27a6c3f968eea0dc7a5cb55d66b6d99224d3e06
Simon Morley <simon@polkaspots.com>
2017-03-07 17:04:51 +0100

    more stuff

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import argparse
 import json
 import pandas as pd
+import struct
+import array
+import base64
 
 from google.cloud import bigtable
 from google.cloud.bigtable.row_filters import ColumnQualifierRegexFilter
 from google.cloud.bigtable.row_filters import RowFilterChain
+from google.cloud.bigtable.row_filters import ValueRangeFilter
+from google.cloud.bigtable.row_filters import ValueRegexFilter
+
 from pandas.io.json import json_normalize
 from matplotlib import pyplot
 
@@ -13,19 +19,16 @@ def main(project_id, instance_id, table_id):
     instance = client.instance(instance_id)
     table = instance.table(table_id)
     column_family_id = 'ST'
-    # column_id = 'TX_BYTES'.encode('utf-8')
 
-    print('Getting a single greeting by row key.')
-
-    # [START scanning_all_rows]
-    print('Scanning for all greetings:')
+    print('Scanning tables:')
 
     col1_filter = ColumnQualifierRegexFilter(b'TX_BYTES:([a-zA-Z0-9]{12})')
-    # col1_filter = ColumnQualifierRegexFilter(b'TX_BYTES:7081EB6E6051')
-    # chain1 = RowFilterChain(filters=[col1_filter])
+    col2_filter = ValueRegexFilter('11820581')
+    col3_filter = ValueRangeFilter("0".encode('utf-8'), "1488784881000".encode('utf-8'))
 
-    # partial_rows = table.read_rows()
-    partial_rows = table.read_rows(filter_=col1_filter)
+    chain1 = RowFilterChain(filters=[col1_filter, col2_filter, col3_filter])
+
+    partial_rows = table.read_rows(filter_=chain1)
     partial_rows.consume_all()
 
     a = []
@@ -38,6 +41,27 @@ def main(project_id, instance_id, table_id):
         val = { "Date": cell.timestamp.strftime("%a, %d %b %Y %H:%M:%S"), "Value": float(value) }
         a.append(val)
 
+
+    # print(a)
+
+    column_family_id = 'CAPS'
+
+    partial_rows = table.read_rows()
+    partial_rows.consume_all()
+
+    for row_key, row in partial_rows.rows.items():
+        key = row_key.decode('utf-8')
+
+        cell = row.cells[column_family_id]['CAPS_2'][0]
+
+        a = struct.unpack(">ll", cell.value)[1]
+
+        print(a)
+
+        # cell = cell[cell.keys()[0]][0]
+        # value = cell.value.decode('utf-8')
+        # val = { "Date": cell.timestamp.strftime("%a, %d %b %Y %H:%M:%S"), "Value": float(value) }
+        # a.append(val)
 
     print(a)
 
